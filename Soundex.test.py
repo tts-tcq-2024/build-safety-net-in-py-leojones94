aab13dbc4b7e851ed5a05a683baa3aab02d89aeb
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-07-05 00:14:39 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,13 +1,19 @@
 import unittest
-from Soundex import generate_soundex
+from soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
+    
+    def test_no_argument(self):
+        self.assertEqual(generate_soundex(), "")
 
     def test_empty_string(self):
         self.assertEqual(generate_soundex(""), "")
 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
+
+    def test_vowel_separated_character(self):
+        self.assertEqual(generate_soundex("Tymczak"), "T522")
 
     
 if __name__ == '__main__':
